ed1229b78d4928ded2b101d97bfc8cd3b3bb63f9
Sylvain Prevost <admin@WIN10-VM.vrstx.com>
2016-12-21 14:08:38 -0600

    add .props file to control target project compile/link settings.

--- build_hololens_uwp.bat
+++ build_hololens_uwp.bat
@@ -1,6 +1,10 @@
 set Basedir=%~dp0
 set ContribModulesDir="%Basedir%..\opencv_contrib-hololens\modules"
 
+rmdir /S /Q Junk
+rmdir /S /Q CMakeFiles
+mkdir bin/WS/10.0/x86
+
 cd %Basedir%\platforms\winrt
 cmd /C setup_winrt "WS" "10.0" "x86" "OFF" -g "Visual Studio 14 2015" -c %ContribModulesDir%
 cd %Basedir%
